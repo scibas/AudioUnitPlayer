5a94863aad91bca9ea10c9db6bcfb457265f96c5
Pawel Scibek <pawel@Pawels-Mac-mini.local>
2018-01-14 12:00:24 +0100

    simple UI added

--- AudioUnitSample/MainView.swift
+++ AudioUnitSample/MainView.swift
@@ -1,21 +1,33 @@
-//
-//  MainView.swift
-//  AudioUnitSample
-//
-//  Created by Pawel Scibek on 14/01/2018.
-//  Copyright © 2018 Pawel Scibek. All rights reserved.
-//
-
 import UIKit
 
 class MainView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    lazy var playButton: UIButton = {
+        let playButton = UIButton()
+        playButton.setTitle("Play", for: .normal)
+        playButton.setTitleColor(.blue, for: .normal)
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+        return playButton
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        addSubview(playButton)
+        
+        setupCustomConstraints()
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCustomConstraints() {
+        playButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        playButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        playButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        playButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
 }
