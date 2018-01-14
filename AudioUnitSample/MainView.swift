8950df58e6b842c784a59427b7347060e8a0df56
Pawel Scibek <pawel@Pawels-Mac-mini.local>
2018-01-14 11:59:57 +0100

    simple UI added

---
+++ AudioUnitSample/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  AudioUnitSample
+//
+//  Created by Pawel Scibek on 14/01/2018.
+//  Copyright © 2018 Pawel Scibek. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ AudioUnitSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioUnitSample
+//
+//  Created by Pawel Scibek on 14/01/2018.
+//  Copyright © 2018 Pawel Scibek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
